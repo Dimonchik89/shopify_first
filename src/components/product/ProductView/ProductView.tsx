9c87396fd486b743a675505e20b2a0930ad18737
Dmitriy-mn <dmitriy.k.89.mn@gmail.com>
2024-07-10 12:06:09 +0300

    create switch and helpers for find selected variant products

--- src/components/product/ProductView/ProductView.tsx
+++ src/components/product/ProductView/ProductView.tsx
@@ -7,77 +7,102 @@ import { Button, Container } from '@components/ui';
 import { Product } from '@common/types/product';
 import Image from 'next/image';
 import ProductSlider from '../ProductSlider';
+import Swatch from '../Swatch';
+import { Choices, getVariant } from '../helpers';
+import { useUi } from '@components/ui/context';
 
 interface Props {
-    product: Product;
+  product: Product;
 }
 
 const ProductView: FC<Props> = ({ product }) => {
-    return (
-        <Container>
-            <div className={cn(s.root, 'fit', 'mb-5')}>
-                <div
-                    className={cn(s.productDisplay, 'fit')}
-                >
-                    <div className={s.nameBox}>
-                        <h1 className={s.name}>
-                            {product.name}
-                        </h1>
-                        <div className={s.price}>
-                            {product.price.value}
-                            {` `}
-                            {product.price.currencyCode}
-                        </div>
-                    </div>
-                    <ProductSlider>
-                        {product.images.map((image) => (
-                            <div
-                                key={image.url}
-                                className={s.imageContainer}
-                            >
-                                <Image
-                                    className={s.img}
-                                    src={image.url}
-                                    alt={image.alt ?? ''}
-                                    width={1050}
-                                    height={1050}
-                                    quality="85"
-                                />
-                            </div>
-                        ))}
-                    </ProductSlider>
-                </div>
-                <div className={s.sidebar}>
-                    <section>
-                        {product.options.map((option) => (
-                            <div
-                                key={option.id}
-                                className="pb-4"
-                            >
-                                <h2 className="uppercase font-medium">
-                                    {option.displayName}
-                                </h2>
-                                <div className="flex flex-row py-4"></div>
-                            </div>
-                        ))}
-                        <div className="pb-14 break-words w-full max-w-xl text-lg">
-                            {product.description}
-                        </div>
-                    </section>
-                    <div>
-                        <Button
-                            className={s.button}
-                            onClick={() => {
-                                alert('click add to cart');
-                            }}
-                        >
-                            Add to Cart
-                        </Button>
-                    </div>
-                </div>
+  const [choices, setChoices] = useState<Choices>({});
+  const { openSidebar } = useUi();
+
+  const variant = getVariant(product, choices);
+
+  const addToCart = () => {
+    try {
+      const item = {
+        productId: String(product.id),
+        variantId: variant?.id,
+        variantOptions: variant?.options,
+      };
+
+      alert(JSON.stringify(item));
+      openSidebar();
+    } catch (error) {}
+  };
+
+  return (
+    <Container>
+      <div className={cn(s.root, 'fit', 'mb-5')}>
+        <div className={cn(s.productDisplay, 'fit')}>
+          <div className={s.nameBox}>
+            <h1 className={s.name}>{product.name}</h1>
+            <div className={s.price}>
+              {product.price.value}
+              {` `}
+              {product.price.currencyCode}
             </div>
-        </Container>
-    );
+          </div>
+          <ProductSlider>
+            {product.images.map((image) => (
+              <div key={image.url} className={s.imageContainer}>
+                <Image
+                  className={s.img}
+                  src={image.url}
+                  alt={image.alt ?? ''}
+                  width={1050}
+                  height={1050}
+                  quality="85"
+                />
+              </div>
+            ))}
+          </ProductSlider>
+        </div>
+        <div className={s.sidebar}>
+          <section>
+            {product.options.map((option) => (
+              <div key={option.id} className="pb-4">
+                <h2 className="uppercase font-medium">{option.displayName}</h2>
+                <div className="flex flex-row py-4">
+                  {option.values.map((optValue) => {
+                    const activeChoice =
+                      choices[option.displayName.toLowerCase()];
+
+                    return (
+                      <Swatch
+                        key={`${option.id}-${optValue.label}`}
+                        label={optValue.label}
+                        color={optValue.hexColor}
+                        active={optValue.label.toLowerCase() === activeChoice}
+                        variant={option.displayName}
+                        onClick={() => {
+                          setChoices({
+                            ...choices,
+                            [option.displayName.toLowerCase()]: optValue.label,
+                          });
+                        }}
+                      />
+                    );
+                  })}
+                </div>
+              </div>
+            ))}
+            <div className="pb-14 break-words w-full max-w-xl text-lg">
+              {product.description}
+            </div>
+          </section>
+          <div>
+            <Button className={s.button} onClick={addToCart}>
+              Add to Cart
+            </Button>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
 };
 
 export default ProductView;
--- src/components/ui/context.tsx
+++ src/components/ui/context.tsx
@@ -1,88 +1,76 @@
 'use client';
 
 import {
-    createContext,
-    FC,
-    PropsWithChildren,
-    useContext,
-    useMemo,
-    useReducer,
-    useState,
+  createContext,
+  FC,
+  PropsWithChildren,
+  useContext,
+  useMemo,
+  useReducer,
 } from 'react';
 
 interface StateModifiers {
-    openSidebar: () => void;
-    closeSidebar: () => void;
+  openSidebar: () => void;
+  closeSidebar: () => void;
 }
 
 interface StateValues {
-    isSidebarOpen: boolean;
+  isSidebarOpen: boolean;
 }
 
 const stateModifiers = {
-    openSidebar: () => {},
-    closeSidebar: () => {},
+  openSidebar: () => {},
+  closeSidebar: () => {},
 };
 
 const initialState = {
-    isSidebarOpen: false,
+  isSidebarOpen: false,
 };
 
 type State = StateModifiers & StateValues;
 
 const UIContext = createContext<State>({
-    ...initialState,
-    ...stateModifiers,
+  ...initialState,
+  ...stateModifiers,
 });
 
 type Action = { type: 'OPEN_SIDEBAR' | 'CLOSE_SIDEBAR' };
 
 function uiReducer(state: StateValues, action: Action) {
-    switch (action.type) {
-        case 'OPEN_SIDEBAR': {
-            return {
-                ...state,
-                isSidebarOpen: true,
-            };
-        }
-        case 'CLOSE_SIDEBAR': {
-            return {
-                ...state,
-                isSidebarOpen: false,
-            };
-        }
+  switch (action.type) {
+    case 'OPEN_SIDEBAR': {
+      return {
+        ...state,
+        isSidebarOpen: true,
+      };
     }
+    case 'CLOSE_SIDEBAR': {
+      return {
+        ...state,
+        isSidebarOpen: false,
+      };
+    }
+  }
 }
 
-export const UiProvider: FC<PropsWithChildren> = ({
-    children,
-}) => {
-    const [state, dispatch] = useReducer(
-        uiReducer,
-        initialState
-    );
+export const UiProvider: FC<PropsWithChildren> = ({ children }) => {
+  const [state, dispatch] = useReducer(uiReducer, initialState);
 
-    const openSidebar = () =>
-        dispatch({ type: 'OPEN_SIDEBAR' });
-    const closeSidebar = () =>
-        dispatch({ type: 'CLOSE_SIDEBAR' });
+  const openSidebar = () => dispatch({ type: 'OPEN_SIDEBAR' });
+  const closeSidebar = () => dispatch({ type: 'CLOSE_SIDEBAR' });
 
-    const value = useMemo(() => {
-        return {
-            ...state,
-            openSidebar,
-            closeSidebar,
-        };
-    }, [state.isSidebarOpen]);
+  const value = useMemo(() => {
+    return {
+      ...state,
+      openSidebar,
+      closeSidebar,
+    };
+  }, [state.isSidebarOpen]);
 
-    return (
-        <UIContext.Provider value={value}>
-            {children}
-        </UIContext.Provider>
-    );
+  return <UIContext.Provider value={value}>{children}</UIContext.Provider>;
 };
 
 export const useUi = () => {
-    const context = useContext(UIContext);
-    return context;
+  const context = useContext(UIContext);
+  return context;
 };
